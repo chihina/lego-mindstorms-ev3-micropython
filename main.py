af23d085fbb3d3751083e05d38351e157aa82c48
Chihiro Nakatani <program.student.tti@gmail.com>
2020-12-19 11:49:32 +0900

    rewrite comment

--- main.py
+++ main.py
@@ -15,28 +15,27 @@ from time import gmtime
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
 # Click "Open user guide" on the EV3 extension tab for more information.
 
-# 色判定メソッド (1:red, 2:green, 3:blue)
 def color_checker(color_sensor):
-    
-    # ex. (x, y, z) 0-100 の反射値を出す
-    rgb_num_list = list(map(int, color_sensor.rgb()))
-    print(rgb_num_list)
+     """This function is color checker to choose a correct color marble.
 
-    max_num = max(rgb_num_list)
-    max_idx = rgb_num_list.index(max_num) + 1
+    Check color labels by using lego color sensor.
+    The color sensor is very sensitive so that we need to use it in the same light condtion.
 
-    return max_idx
+    Parameters
+    ----------
+    color_sensor : Color sensor instance
+        This is a color sensor instance.
 
-# 色判定メソッド (1:red, 2:green, 3:blue, 4:オレンジ, 5:空, 6:怪しいのでパス)
-def color_checker_fukaya(color_sensor):
-    
-    # ex. (x, y, z) 0-100 の反射値を出す
-    rgb_num_list = list(map(int, color_sensor.rgb()))
-    r_num, g_num, b_num = map(int, rgb_num_list)
+    Returns
+    -------
+    Color number
+        1:Red, 2:Green, 3:Blue, 4:Orange, 5:Empty, 6:Pass
+        Pass label means that the check is suspicious.
+    """
+
+    r_num, g_num, b_num = map(int, color_sensor.rgb())
     print(rgb_num_list, end=":")
 
-    # ---------------------------色判定部-----------------------------------------
-    # 反射値が大きいときはオレンジに近い?
     if (r_num+g_num+b_num) > 35:
         print("Orange")
         return 4
@@ -54,240 +53,179 @@ def color_checker_fukaya(color_sensor):
         print("Blue")
         return 3
     elif max(rgb_num_list) == 1 and not max(rgb_num_list) == 0:
-        print("pass (Blue)")
-        # return 6
+        print("Blue")
         return 3
     else:
         print("Empty")
         return 5
-    # ---------------------------色判定部-----------------------------------------
 
-# 落としたい時の40度回転(ゆっくり回転)
-def motor_forty_degree_late(motor):
-    motor.run_angle(50, 38, Stop.BRAKE)
+def rotate_thirty_degrees_fast(motor):
+    """This function is used to rotate a gear 30 degrees.
 
-# 落としたくない時の40度回転(早く回転)
-def motor_forty_degree_slow(motor):
-    motor.run_angle(300, 38, Stop.BRAKE)
+    We use this function when we don’t want to drop a marble which checked by color sensor.
+    The motor has several degrees error. We need to think about it.
 
-# 落としたくない時の60度回転(早く回転)
-def motor_sixty_degree_fast(motor):
-    # print("Rotate 60 degrees")
+    Parameters
+    ----------
+    motor : Motor instance
+        This is a motor instance.
+    """
     motor.run_angle(600, 32, Stop.BRAKE)
 
-# Empty empty 用
-def motor_empty_degree_fast(motor):
-    # print("Rotate 60 degrees")
+def rotate_twenty_degrees_fast(motor):
+    """This function is used to rotate a gear 20 degrees.
+
+    We use this function when we want to skip the empty box in a gear.
+    By using this function, We improved speed. 
+    The motor has several degrees error. We need to think about it.
+
+    Parameters
+    ----------
+    motor : Motor instance
+        This is a motor instance.
+    """
     motor.run_angle(600, 22, Stop.BRAKE)
 
-# 5度回転
-def five_degree_lotate(motor):
-    # print("Rotate 5 degree")
+def rotate_five_degrees_fast(motor):
+    """This function is used to rotate a gear 5 degrees.
+
+    We use this function when we want to drop a marble which checked by color sensor.
+    The motor has several degrees error. We need to think about it.
+
+    Parameters
+    ----------
+    motor : Motor instance
+        This is a motor instance.
+    """
     motor.run_angle(600, 5, Stop.BRAKE)
 
-# 10度回転
-def ten_degree_lotate(motor):
-    motor.run_angle(400, 10, Stop.BRAKE)
+def drop_all_marbles(motor):
+    """This function is used to drop all marbles.
 
-# ボール全排出用の関数(デバッグ用)
-def departure_ball(motor):
+    We use this function when we droped a marble into the 8th box.
+    We don't need to check the last marble. By using this function, We improved speed. 
+
+    Parameters
+    ----------
+    motor : Motor instance
+        This is a motor instance.
+    """
     motor.run_angle(100, 360, Stop.BRAKE)
 
-# メイン関数
+def fill_marbles_into_gear(motor):
+    """This function is used to rotate 160 degrees gear.
+
+    We use this function when we start program.
+    By using this function, We improved speed. 
+
+    Parameters
+    ----------
+    motor : Motor instance
+        This is a motor instance.
+    """
+    motor.run_angle(50, 160, Stop.BRAKE)
+
 def main():
+    """This is a main function of the script.
 
-    print("Exe main function")
+    We archied a high performance by calling this function which is refined.
+
+    """
     
-    # ev3 のインスタンスを生成する
+    # Generate connection with lego mindstorms
     ev3 = EV3Brick()
-
-    # 選別開始のアラームを鳴らす
     ev3.speaker.beep()
-
-    # モーターとカラーセンサのインスタンスを生成
-    motor = Motor(port=Port.A)
-    color_sensor = ColorSensor(port=Port.S1)
-
-    # 正解のビー玉の順番を与える (1:red, 2:green, 3:blue)
-    color_list = [1, 3, 2, 3, 2, 1, 2, 1, 3]
-    
-    # 埋まっているかどうかのフラグを保存するリスト
-    box_flag_list = [True for _ in range(9)]
-    
-    while True:        
-        # すべての場所が False (=0) になれば ループを抜ける
-        if sum(box_flag_list) == 0:
-            break
-        
-        # それぞれの場所について考える
-        for box_idx, box_flag in enumerate(box_flag_list):
-            
-            # 最初に埋まっていない場所なら
-            if box_flag:
-
-                # 次に来てほしい色を取得
-                true_color = color_list[box_idx]
-
-                # 判定した色を取得
-                jud_color = color_checker(color_sensor)
-
-                # もし, 排出したい色ならば
-                if jud_color == true_color:
-
-                    # 埋まっている場所を False にする
-                    box_flag_list[box_idx] = False
-                    
-                    # モーターを 40度回転(落としたい時)
-                    motor_forty_degree_late(motor)
-                    
-                    # インターバル 1秒
-                    time.sleep(1)
-
-                # ちがうならパス
-                else:
-                    # モーター 40度回転(落としたくない時)
-                    motor_forty_degree_fast(motor)
-
-                    # インターバル 1秒
-                    time.sleep(1)
-                break
-
-    # 仕分け終了のアラーム
-    ev3.speaker.beep()
-
-# メイン関数(バージョン2)
-def main_two():
-
-    # ev3 のインスタンスを生成する
-    ev3 = EV3Brick()
-
-    # 選別開始のアラームを鳴らす
-    ev3.speaker.beep()
-
-    # モーターとカラーセンサのインスタンスを生成
     motor = Motor(port=Port.A)
     color_sensor = ColorSensor(port=Port.S4)
 
-    # 正解のビー玉の順番を与える (1:red, 2:green, 3:blue)
+    # Define the list of color order which we should sort
     color_list = [1, 3, 2, 3, 2, 1, 2, 1, 3]
 
-    # 埋まっているかどうかのフラグを保存するリスト
+    # Define the list of flags whether each box include a marble or not  
     box_flag_list = [True for _ in range(9)]
     
-    # 同じ色を連続で判定する問題用フラグ
+    # Define a variable to save the previous checked color
     same_color_var = 0
 
-    # 最初にある程度回しておく
-    motor.run_angle(50, 160, Stop.BRAKE)
+    # Rotate 160 degrees gear
+    fill_marbles_into_gear(motor)
 
     while True:        
-        print(box_flag_list)
 
-        # すべての場所が False (=0) になれば ループを抜ける
+        # Get out of the loop when we drop a marble into 8th box
         if sum(box_flag_list) == 1:
             departure_ball(motor)
             break
         
-        five_degree_lotate(motor)
+        # Rotate 5 degrees gear and sleep few second
+        rotate_five_degrees_fast(motor)
         time.sleep(0.6)
 
-        # それぞれの場所について考える
-        for box_idx, box_flag in enumerate(box_flag_list):
-            
-            # 最初に埋まっていない場所なら
-            if box_flag:
+        # Get the color label of the box we want to fill
+        box_idx = box_flag_list.index(True)
+        true_color = color_list[box_idx]
+        checked_color = color_checker(color_sensor)
+        
+        # If the checked color is the same as the previous decision, we skip it.
+        if same_color_var == checked_color and checked_color in [1, 2, 3]:
+            pass
 
-                # 次に来てほしい色を取得
-                true_color = color_list[box_idx]
+        # If we check empty twice in the row, rotate about 22 degrees
+        elif same_color_var == checked_color and checked_color == 5:
+            rotate_twenty_degrees_fast(motor)
 
-                # 判定した色を取得
-                jud_color = color_checker_fukaya(color_sensor)
-                
-                # もし連続で同じ色を判定したら
-                if same_color_var == jud_color and jud_color in [1, 2, 3]:
-                    # time.sleep(0.2)
-                    pass
+        # If the checked color is the correct color, we drop the marble in the box
+        elif checked_color == true_color:
+            box_flag_list[box_idx] = False
 
-                # 2回数連続 empty なら
-                elif same_color_var == jud_color and jud_color == 5:
-                    print("Empty empty !!!")
-                    motor_empty_degree_fast(motor)
+            for _ in range(3):
+                rotate_five_degrees_fast(motor)
 
-                # もし, 排出したい色ならば
-                elif jud_color == true_color:
-                    # 該当の場所を False にする
-                    box_flag_list[box_idx] = False
+            # reset a saved color label
+            same_color_var = 0
 
-                    # パス対策
-                    for _ in range(3):
-                        five_degree_lotate(motor)
+        # If the checked color is orange, we skip it
+        elif checked_color == 4:
+            pass
 
-                    # 落としたらリセット
-                    same_color_var = 0
+        # If the checked color is empty, we skip it
+        elif checked_color == 5:
+            pass
+        
+        # If the checked color is suspecious, we don't drop it
+        elif checked_color == 6:
+            rotate_thirty_degrees_fast(motor)
+            time.sleep(0.5)
 
-                # 判定がオレンジなら
-                elif jud_color == 4:
-                    # time.sleep(0.2)
-                    pass
+        # If the checked color is color we don't expect, rotate 60 degrees gear in fast
+        else:
+            rotate_thirty_degrees_fast(motor)
+            time.sleep(0.5)
 
-                # 判定が空なら
-                elif jud_color == 5:
-                    # time.sleep(0.2)
-                    pass
-                
-                # 怪しい判定ならパスする
-                elif jud_color == 6:
-                    # モーター 60度回転(落としたくない時)
-                    motor_sixty_degree_fast(motor)
+            # reset a saved color label
+            same_color_var = 0
 
-                    # インターバル 1秒
-                    time.sleep(0.5)
-
-                # 落としたくない色がきたら
-                else:
-                    # モーター 60度回転(落としたくない時)
-                    motor_sixty_degree_fast(motor)
-
-                    # インターバル 1秒
-                    time.sleep(0.5)
-
-                    # 回転させたらリセット
-                    same_color_var = 0
-
-                    break
-
-                same_color_var = jud_color
-
-                break
-
-    # 仕分け終了のアラーム
-    ev3.speaker.beep()
+        # save the color label checked in this iteration for next iteration
+        same_color_var = checked_color
 
 if __name__ == "__main__":
 
-    # 選別開始のアラーム
-    print("Start !!")
-
-    # 開始時間の記録
+    # Save the start time
     start_time = time.time()
 
-    # ev3 のインスタンスを生成
+    # Generate instance of EV3
     ev3 = EV3Brick()
 
-    # 選別の開始のアラーム
+    # Start alert
+    print("Start !!")
     ev3.speaker.beep()
 
-    # モーターとカラーセンサのインスタンスを生成
-    motor = Motor(port=Port.A)
-    color_sensor = ColorSensor(port=Port.S4)
-
-    # 深谷ver
-    main_two()
-
-    # departure_ball(motor)
+    # Start a main function
+    main()
     
-    # 選別終了のアラーム
+    # finish alert 
     print("Finish !!")
     ev3.speaker.play_file(SoundFile.GOOD_JOB)
 
+    # output the time we used to sort marbles
     print(strftime("%H:%M:%S", gmtime(time.time() - start_time)))
